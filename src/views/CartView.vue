9fb33302a4bbd66774093d226af6b874fe9505cd
Dmitry Nishcheta <dnishcheta@gmail.com>
2025-01-14 02:04:08 +0500

    List transition

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -3,21 +3,23 @@
         <h2>Корзина</h2>
         <div v-if="cart.length === 0">Корзина пуста</div>
         <ul v-else>
-            <li v-for="item in cart" :key="item.id">
-                <StoreCard :cover="item.image" row>
-                    <template #title>{{ item.title }}</template>
-                    <p>{{ item.price }} eur.</p>
-
-                    <template #actions>
-                        <div style="display: flex; justify-content: space-between; align-items: center; gap: 0.5rem;">
-                            <StoreButton @click="updateQuantity(item.id, item.quantity - 1)">-</StoreButton>
-                            {{ item.quantity }}
-                            <StoreButton @click="updateQuantity(item.id, item.quantity + 1)">+</StoreButton>
-                        </div>
-                        <StoreButton @click="removeItem(item.id)">Удалить</StoreButton>
-                    </template>
-                </StoreCard>
-            </li>
+            <TransitionGroup name="list">
+                <li v-for="item in cart" :key="item.id">
+                    <StoreCard :cover="item.image" row>
+                        <template #title>{{ item.title }}</template>
+                        <p>{{ item.price }} eur.</p>
+                        <template #actions>
+                            <div
+                                style="display: flex; justify-content: space-between; align-items: center; gap: 0.5rem;">
+                                <StoreButton @click="updateQuantity(item.id, item.quantity - 1)">-</StoreButton>
+                                {{ item.quantity }}
+                                <StoreButton @click="updateQuantity(item.id, item.quantity + 1)">+</StoreButton>
+                            </div>
+                            <StoreButton @click="removeItem(item.id)">Удалить</StoreButton>
+                        </template>
+                    </StoreCard>
+                </li>
+            </TransitionGroup>
         </ul>
         <hr />
         <div>
