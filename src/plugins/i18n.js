3abfbc01a833c1bcda914e9335fe70728906d3e0
Dmitry Nishcheta <dnishcheta@gmail.com>
2025-01-14 18:49:38 +0500

    Prepare

---
+++ src/plugins/i18n.js
@@ -0,0 +1,32 @@
+const locales = {
+    ru: {
+        'welcome': 'Добро пожаловать на сайт',
+        'username': 'Имя пользователя',
+        'password': 'Пароль',
+        'login': 'Войти',
+        'logout': 'Выйти',
+        'error': 'Произошла ошибка:',
+        'catalog': 'Каталог',
+        'cart': 'Корзина',
+        'add-to-cart': 'Добавить в корзину',
+        'total': 'Итого',
+        'delete': 'Удалить',
+    },
+    en: {
+        'welcome': 'Welcome to the awesome store',
+        'username': 'Username',
+        'password': 'Password',
+        'login': 'Login',
+        'logout': 'Logout',
+        'error': 'Error:',
+        'catalog': 'Catalog',
+        'cart': 'Cart',
+        'add-to-cart': 'Add to cart',
+        'total': 'Total',
+        'delete': 'Delete',
+    },
+}
+
+export const i18nPlugin = {
+    
+}
