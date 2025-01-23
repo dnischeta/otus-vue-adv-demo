8a77c5788890466b81cfdb157db767740c8dbad5
Dmitry Nishcheta <dnishcheta@gmail.com>
2025-01-23 23:30:26 +0500

    .

--- src/main.js
+++ src/main.js
@@ -18,9 +18,9 @@ enableMocking().then(() => {
 })
 
 async function enableMocking() {
-    if (process.env.NODE_ENV !== 'development') {
-        return
-    }
+    // if (process.env.NODE_ENV !== 'development') {
+        // return
+    // }
     const { worker } = await import('./mocks/index.js')
     return worker.start({ serviceWorker: { url: `${import.meta.env.BASE_URL}/mockServiceWorker.js` } })
 }
